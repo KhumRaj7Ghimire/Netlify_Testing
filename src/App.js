98333551360fff866dd73bc5af4c361365a3cd7f
KhumRaj7Ghimire <khumraj7ghimire@gmail.com>
2021-03-04 15:17:25 +0545

    First commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import "antd/dist/antd.css";
+
+const App = () => {
+  return <div className="main-container">Successfully Deployed</div>;
+};
+
+export default App;
---
+++ src/Component/MenuData.js
@@ -0,0 +1,97 @@
+const DATA = [
+  {
+    id: 1,
+    categoryName: "Men",
+    subCategory: [
+      {
+        id: 1,
+        subCategoryName: "Topi / Nepali Hat",
+      },
+      {
+        id: 2,
+        subCategoryName: "Vests",
+      },
+      {
+        id: 1,
+        subCategoryName: "Kurta Set",
+      },
+      {
+        id: 1,
+        subCategoryName: "Daura Surwal",
+      },
+    ],
+  },
+  {
+    id: 2,
+    categoryName: "Women",
+    subCategory: [
+      {
+        id: 1,
+        subCategoryName: "Saree",
+      },
+      {
+        id: 2,
+        subCategoryName: "Kurti",
+      },
+      {
+        id: 3,
+        subCategoryName: "Jewelry",
+      },
+      {
+        id: 4,
+        subCategoryName: "Shoes",
+      },
+      {
+        id: 5,
+        subCategoryName: "Accessories",
+      },
+    ],
+  },
+  {
+    id: 3,
+    categoryName: "Kids",
+    subCategory: [
+      {
+        id: 1,
+        subCategoryName: "Pasni Dress",
+      },
+    ],
+  },
+  {
+    id: 4,
+    categoryName: "Puja Items",
+  },
+  {
+    id: 5,
+    categoryName: "Food and Grocery",
+    subCategory: [
+      {
+        id: 1,
+        subCategoryName: "Chau Chau (Noodles)",
+      },
+      {
+        id: 2,
+        subCategoryName: "Nepali Pickles / Aachar",
+      },
+      {
+        id: 3,
+        subCategoryName: "Beaten Rice Nepali",
+      },
+      {
+        id: 4,
+        subCategoryName: "Bhatmas",
+      },
+      {
+        id: 5,
+        subCategoryName: "Titaura - Nepali Candy",
+      },
+    ],
+  },
+
+  {
+    id: 6,
+    categoryName: "New Year Special",
+  },
+];
+
+export default DATA;
